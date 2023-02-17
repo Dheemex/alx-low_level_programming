115e50ff9aca6f1a8af3ce8451b803b3e1fbae68
dheemex <davidoladipo28@gmail.com>
2023-02-16 16:41:45 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,8 +4,8 @@
  * main - A C program that prints exactly and that piece of art is useful
  * Return: 1
  */
-int main (void)
+int main(void)
 {
-	write(1,"and that piece of art is useful" - Dora Korpar, 2015-10-19/n", 59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
