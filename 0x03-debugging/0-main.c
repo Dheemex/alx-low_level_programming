a78d73c49522fc3718dc70e32e553ae7e1ef05c1
dheemex <davidoladipo28@gmail.com>
2023-02-22 18:05:14 -0800

    V

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * main - tests function that prints if leedger is positive or negative
+ * main - tests function that prints positive or negative
  * Return: 0
  */
+
 int main(void)
 {
 	int i;
@@ -11,5 +12,5 @@ int main(void)
 	i = 0;
 	positive_or_negative(i);
 
-	return (0);
+return (0);
 }
