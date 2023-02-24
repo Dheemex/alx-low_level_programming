8097bb5b780d48c5d63708a3c0202d37a6be0baa
dheemex <davidoladipo28@gmail.com>
2023-02-23 17:36:27 -0800

    0x04. C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int_putchar(charc);
+int_isupper(intc);
+int_isdigit(intc);
+intmul(inta,intb);
+voidprint_numbers(void);
+voidprint_most_numbers(void);
+voidmore_numbers(void);
+voidprint_line(intn);
+voidprint_diagonal(intn);
+voidprint_square(intsize);
+voidprint_triangle(intsize);
+voidprint_number(intn);
+
+#endif
