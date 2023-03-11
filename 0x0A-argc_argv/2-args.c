bf9209fef9c42cb0670160ea21f62e88adcea2b5
dheemex <davidoladipo28@gmail.com>
2023-03-10 23:29:38 -0800

    update

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -8,19 +8,13 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(int argc, char *argv[])
 {
-/*Declaring variables*/
-int count = 0
+	int i;
 
-if (argc > 0)
-{
-/*WHILE - Print each argument*/
-while (count < argc)
-{
-printf("%s\n", argv[count]);
-}
-}
-return (0);
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
 }
